70426a34d782ed2b8d992382864f072219975bc6
wangyipengw1p <1014625143@qq.com>
2019-05-25 15:20:28 +0800

    Update binary2BCD.vhd

--- Binary_to_BCD/binary2BCD.vhd
+++ Binary_to_BCD/binary2BCD.vhd
@@ -15,9 +15,6 @@ entity binary2BCD is
 end binary2BCD;
 
 architecture structural of binary2BCD is 
-
-
-
   
 begin  
 
@@ -39,48 +36,7 @@ for i in 0 to 4 loop
 end loop;
  BCD_out <= stack(17 downto 8);
 end process;
---variable high4 : std_logic_vector(9 downto 0):="0000000000";
---variable low4 :  std_logic_vector(9 downto 0):="0000000000";
---begin
---    case binary_in(7 downto 4) is
---        when "0000" =>high4(8 downto 4):= "00000";      
---        when "0001" =>high4(8 downto 4):= "00001";      
---        when "0010" =>high4(8 downto 4):= "00010";      
---        when "0011" =>high4(8 downto 4):= "00011";      
---        when "0100" =>high4(8 downto 4):= "00100";      
---        when "0101" =>high4(8 downto 4):= "00101";      
---        when "0110" =>high4(8 downto 4):= "00110";      
---        when "0111" =>high4(8 downto 4):= "00111";      
---        when "1000" =>high4(8 downto 4):= "01000";      
---        when "1001" =>high4(8 downto 4):= "01001";      
---        when "1010" =>high4(8 downto 4):= "10000";      
---        when "1011" =>high4(8 downto 4):= "10001";      
---        when "1100" =>high4(8 downto 4):= "10010";      
---        when "1101" =>high4(8 downto 4):= "10011";      
---        when "1110" =>high4(8 downto 4):= "10100";      
---        when others =>high4(8 downto 4):= "10101";
---    end case;
-    
---    case binary_in(3 downto 0) is
---        when "0000" =>low4(4 downto 0):= "00000";      
---        when "0001" =>low4(4 downto 0):= "00001";      
---        when "0010" =>low4(4 downto 0):= "00010";      
---        when "0011" =>low4(4 downto 0):= "00011";      
---        when "0100" =>low4(4 downto 0):= "00100";      
---        when "0101" =>low4(4 downto 0):= "00101";      
---        when "0110" =>low4(4 downto 0):= "00110";      
---        when "0111" =>low4(4 downto 0):= "00111";      
---        when "1000" =>low4(4 downto 0):= "01000";      
---        when "1001" =>low4(4 downto 0):= "01001";      
---        when "1010" =>low4(4 downto 0):= "10000";      
---        when "1011" =>low4(4 downto 0):= "10001";      
---        when "1100" =>low4(4 downto 0):= "10010";      
---        when "1101" =>low4(4 downto 0):= "10011";      
---        when "1110" =>low4(4 downto 0):= "10100";      
---        when others =>low4(4 downto 0):= "10101";
---    end case;
-    
---    BCD_out <= high4 + low4;
+
 
 
 end structural;
